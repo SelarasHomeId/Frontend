88e16f0a3b71107cd73899d88a3a5cf12a1a3825
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2024-06-05 08:58:56 +0700

    rebuild #23

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Mengizinkan koneksi dari semua antarmuka
-    port: 8000 // Sesuaikan dengan port yang Anda gunakan
+    host: '0.0.0.0',
+    port: 8000 
   }
 })
